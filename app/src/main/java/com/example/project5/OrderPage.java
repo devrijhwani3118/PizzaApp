253cb6e50623ae26bdb134e4c6cc5e7714bd9805
Akshara Kollu <119714082+SKakshara@users.noreply.github.com>
2024-11-27 12:37:36 -0500

    Added chip group to order page to finish the UI

--- app/src/main/java/com/example/project5/OrderPage.java
+++ app/src/main/java/com/example/project5/OrderPage.java
@@ -1,6 +1,11 @@
 package com.example.project5;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +13,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.chip.ChipGroup;
+
 public class OrderPage extends AppCompatActivity {
 
-
+    private final double basePrice = 8.99;
+    private final double SMALLDELUXE = 16.99;
+    private final double MIDDELUXE = 18.99;
+    private final double LARGEDELUXE = 20.99;
+    private final double SMALLBBQ = 14.99;
+    private final double MIDBBQ = 16.99;
+    private final double LARGEBBQ = 19.99;
+    private final double SMALLMEAT = 17.99;
+    private final double MIDMEAT = 19.99;
+    private final double LARGEMEAT = 21.99;
+    private final double SMALLBUILD = 8.99;
+    private final double MIDBUILD = 10.99;
+    private final double LARGEBUILD = 12.99;
+    private final double TOPPING_COST = 1.69;
+    private final int MAX_TOPPINGS = 7;
+    private double pizzaCost = 0;
+    private Spinner pizzaTypeSpinner;
+    private RadioGroup radioGroup;
+    private TextView crustTextView;
+    private TextView pizzaPriceTextView;
+    private Button addToOrderButton;
+    private ImageView pizzaImageView;
+    private ChipGroup chipGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
